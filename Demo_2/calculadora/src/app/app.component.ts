45e201b54334e411addbe3b785f958438fb99a3e
cmarinca <caro.marinc@gmail.com>
2019-02-12 20:11:58 -0600

    Continuacion calculadora

--- Demo_2/calculadora/src/app/app.component.ts
+++ Demo_2/calculadora/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Operation } from './model/operation';
+import { Operands } from './model/operands';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  current: number = 0;
+  accumulator: number = 0;
+
+  operations: Operation[] = [];
+
+  onSum(): void {
+    this.accumulator += this.current;
+    console.log(this.accumulator);
+    this.operations.push(
+      {
+        operator: this.current,
+        operand: Operands.Sum
+      }
+    );
+  }
+
+  onSub(): void {
+    this.accumulator -= this.current;
+    console.log(this.accumulator);
+    this.operations.push(
+      {
+        operator: this.current,
+        operand: Operands.Sub
+      }
+    );
+  }
+
+  onDiv(): void {
+    this.accumulator /= this.current;
+    console.log(this.accumulator);
+    this.operations.push(
+      {
+        operator: this.current,
+        operand: Operands.Div
+      }
+    );
+  }
+
+  onMul(): void {
+    this.accumulator *= this.current;
+    console.log(this.accumulator);
+    this.operations.push(
+      {
+        operator: this.current,
+        operand: Operands.Mul
+      }
+    );
+  }
+
+  getOperandLabel(op: Operands): string {
+    return Operation.getOperandLabel(op);
+  }
 }
